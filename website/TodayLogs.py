654db6a8f2bd36aab7c76ea52748956e42794b55
Anuraag <anuraag.k.shankar@gmail.com>
2020-01-11 17:55:04 +0530

    Guest Log

--- website/TodayLogs.py
+++ website/TodayLogs.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 import datetime
 from django.contrib.auth.models import User
+from .models import Person, Guest
 from .__init__ import path
 
 path = path + 'website/'
@@ -13,7 +14,16 @@ def TodayLogs():
     Today = pd.DataFrame(columns=['ID','NAME','DATE','TIME','ENTRY/EXIT'])     # NEW DATAFRAME FOR TODAYS LOGS
 
     for p in os.listdir(path+'Logs/'):  # ALL CSVS IN FILE
-        if p.split('.')[-1] == 'csv':
+        if p == 'Guests.csv':
+            csv = pd.read_csv(path + 'Logs/' + p)
+
+            for index in range(csv.shape[0]):
+                if csv.iloc[index]['DATE'] ==Date:  # FOR EACH ENTRY WHERE DATE IS EQUAL TO TODAYS DATE
+                    pk = int(csv.iloc[index]['GUEST ID'].split('g')[-1])
+                    name = Guest.objects.get(pk=pk).name
+                    Today.loc[Today.shape[0]-1] = [csv.iloc[index]['GUEST ID'],name,csv.iloc[index]['DATE'],csv.iloc[index]['TIME'],csv.iloc[index]['ENTRY/EXIT']]
+
+        elif p.split('.')[-1] == 'csv':
             csv = pd.read_csv(path + 'Logs/' + p)
 
             for index in range(csv.shape[0]):
